8cce12d7837ff501a070496a7aabf85c43fd6081
Harsh Rana <107470671+harshrana9027@users.noreply.github.com>
2022-06-30 12:05:43 +0530

    Update Attendance.java

--- Attendance.java
+++ Attendance.java
@@ -33,14 +33,17 @@ public class Attendance
                  Label l2=new Label("ID   : ");
                  Label l3=new Label("TIME : ");
                  Label l4=new Label("DATE : ");
+                             
                  
                  TextField t1=new TextField();
                  TextField t2=new TextField();
                  TextField t3=new TextField();
                  TextField t4=new TextField();
                  
+                 
                  Button b=new Button("Submit");
                  Button b1=new Button("Reset Data");
+                 Button b2=new Button("Back");
                  
                    l.setBounds(140,30,150,40);
                    l1.setBounds(30,100,120,40);
@@ -55,12 +58,73 @@ public class Attendance
                    
                    b.setBounds(100,400,140,50);
                    b1.setBounds(250,400,140,50);
+                   b2.setBounds(190,500,140,50);
+                   
+                   
                    
                    b.addActionListener(new ActionListener()
                    {
                        public void actionPerformed(ActionEvent e)
                        {
-                          f.dispose();
+                          Frame f=new Frame();
+                          Label l=new Label("DATA ENTERED SUCCESSFULLY");
+                          Label l1=new Label();
+                          Label l2=new Label();
+                          Label l3=new Label();
+                          Label l4=new Label();
+                          Button b=new Button("Done");
+                          Button b1=new Button("Back");
+                          
+                          l.setBounds(140,40,250,40);
+                          l1.setBounds(30,100,180,40);
+                          l2.setBounds(30,150,190,40);
+                          l3.setBounds(30,200,190,40);
+                          l4.setBounds(30,250,180,40);
+                          
+                          b.setBounds(100,400,120,40);
+                          b1.setBounds(250,400,120,40);
+                          
+                          f.add(l1);f.add(l2);f.add(l3);f.add(l4);f.add(l);f.add(b);f.add(b1);
+                   
+                          
+                          String s1="Name"+"  :    "+t1.getText();
+                          l1.setText(s1);
+                          String s2="Id"+"    :    "+t2.getText();
+                          l2.setText(s2);
+                          String s3="Time"+"  :    "+t3.getText();
+                          l3.setText(s3);
+                          String s4="Date"+"  :    "+t4.getText();
+                          l4.setText(s4);
+                          
+                          b.addActionListener(new ActionListener()
+                          {
+                              public void actionPerformed(ActionEvent e)
+                              {
+                                  f.dispose();
+                                
+                              }
+                              
+                          });
+                          
+                          b1.addActionListener(new ActionListener()
+                          {
+                              public void actionPerformed(ActionEvent e)
+                              {
+                                   f.dispose();
+                                  
+                              }
+                          });
+                          
+                          f.setSize(500,700);
+                          f.setLayout(null);
+                          f.setVisible(true);
+                          
+                          f.addWindowListener(new WindowAdapter()
+                          {
+                             public void windowClosing(WindowEvent windowEvent){
+                             System.exit(0);
+                           }        
+                         }); 
                            
                        }
                        
@@ -77,13 +141,22 @@ public class Attendance
                        }
                                
                    });
+                   
+                    b2.addActionListener(new ActionListener()
+                   {
+                       public void actionPerformed(ActionEvent e)
+                       {
+                           f.dispose();
+                           
+                       }
+                   });
                   
                    
                    f.add(l1);f.add(l2);f.add(l3);
                    f.add(t1);f.add(t2);f.add(t3);f.add(b);f.add(l);
-                   f.add(l4);f.add(t4);f.add(b1);
+                   f.add(l4);f.add(t4);f.add(b1);f.add(b2);
                    
-                   f.setSize(500,500);
+                   f.setSize(500,700);
                    f.setLayout(null);
                    f.setVisible(true); 
                    
@@ -116,6 +189,7 @@ public class Attendance
                  
                  Button b=new Button("Submit");
                  Button b1=new Button("Reset Data");
+                 Button b2=new Button("Back");
                  
                  l.setBounds(140,30,150,40);
                  l1.setBounds(30,100,120,40);
@@ -131,16 +205,89 @@ public class Attendance
                    
                   b.setBounds(100,400,140,50);
                   b1.setBounds(250,400,140,50);
+                  b2.setBounds(190,500,140,50);
                   
                   b.addActionListener(new ActionListener()
                    {
                        public void actionPerformed(ActionEvent e)
                        {
-                          f.dispose();
+                          Frame f=new Frame();
+                          Label l=new Label("DATA ENTERED SUCCESSFULLY");
+                          Label l1=new Label();
+                          Label l2=new Label();
+                          Label l3=new Label();
+                          Label l4=new Label();
+                          Button b=new Button("Done");
+                          Button b1=new Button("Back");
+                          
+                          l.setBounds(140,40,250,40);
+                          l1.setBounds(30,100,180,40);
+                          l2.setBounds(30,150,190,40);
+                          l3.setBounds(30,200,190,40);
+                          l4.setBounds(30,250,180,40);
+                  
+                           b.setBounds(100,400,120,40);
+                           b1.setBounds(250,400,120,40);
+                          
+                          f.add(l1);f.add(l2);f.add(l3);f.add(l4);f.add(l);f.add(b);f.add(b1);
+                   
+                          
+                          String s1="Name"+"  :    "+t1.getText();
+                          l1.setText(s1);
+                          String s2="Id"+"    :    "+t2.getText();
+                          l2.setText(s2);
+                          String s3="Time"+"  :    "+t3.getText();
+                          l3.setText(s3);
+                          String s4="Date"+"  :    "+t4.getText();
+                          l4.setText(s4);
+                  
+                            b.addActionListener(new ActionListener()
+                          {
+                              public void actionPerformed(ActionEvent e)
+                              {
+                                  f.dispose();
+                                
+                              }
+                              
+                          });
+                          
+                          b1.addActionListener(new ActionListener()
+                          {
+                              public void actionPerformed(ActionEvent e)
+                              {
+                                   f.dispose();
+                                  
+                              }
+                          });
+                          
+                          f.setSize(500,700);
+                          f.setLayout(null);
+                          f.setVisible(true);
+                  
+                          f.addWindowListener(new WindowAdapter()
+                          {
+                             public void windowClosing(WindowEvent windowEvent){
+                             System.exit(0);
+                           }        
+                         }); 
                            
                        }
                        
                    });
+                  
+                   b2.addActionListener(new ActionListener()
+                   {
+                       public void actionPerformed(ActionEvent e)
+                       {
+                           f.dispose();
+                           
+                       }
+                   });
+                  
+                  
+                  
+               
+                 
                   
                   b1.addActionListener(new ActionListener()
                    {
@@ -157,9 +304,9 @@ public class Attendance
                   
                   f.add(l1);f.add(l2);f.add(l3);
                   f.add(t1);f.add(t2);f.add(t3);f.add(b);f.add(l);
-                  f.add(l4);f.add(t4);f.add(b1);
+                  f.add(l4);f.add(t4);f.add(b1);f.add(b2);
         
-                  f.setSize(500,500);
+                  f.setSize(500,700);
                   f.setLayout(null);
                   f.setVisible(true);
                   
@@ -176,7 +323,7 @@ public class Attendance
          
          f.add(b1);f.add(b2);
          
-          f.setSize(500,500);
+          f.setSize(500,700);
           f.setLayout(null);
           f.setVisible(true);
           
