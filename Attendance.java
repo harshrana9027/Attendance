aadbbd8e8785e7cda318e612b4f70053ce9ca701
Harsh Rana <107470671+harshrana9027@users.noreply.github.com>
2022-07-01 11:53:41 +0530

    Update Attendance.java

--- Attendance.java
+++ Attendance.java
@@ -17,12 +17,24 @@ public class Attendance
     public static void main(String args[])
     {
         Frame f=new Frame();
+        
+        f.setBackground(Color.gray);
+        
         Button b1=new Button("USER");
         Button b2=new Button("ADMIN");
         Label l=new Label("Select Type : ");
+        l.setForeground(Color.red);
+                
         
         b1.setBounds(50,120,150,40);
         b2.setBounds(250,120,150,40);
+        
+        b1.setBackground(Color.CYAN);
+        b1.setForeground(Color.black);
+        b2.setBackground(Color.CYAN);
+        b2.setForeground(Color.black);
+        
+
         l.setBounds(200,40,200,40);
         
          b1.addActionListener(new ActionListener()
@@ -30,22 +42,42 @@ public class Attendance
             public void actionPerformed(ActionEvent e)
             {
                  Frame f=new Frame("Attendance");
+                 f.setBackground(Color.lightGray);
+                 
                  Label l=new Label("Attendance Form");
+                 l.setForeground(Color.red);
                  Label l1=new Label("NAME : ");
+                 l1.setForeground(Color.red);
                  Label l2=new Label("ID   : ");
+                 l2.setForeground(Color.red);
                  Label l3=new Label("TIME : ");
+                 l3.setForeground(Color.red);
                  Label l4=new Label("DATE : ");
+                 l4.setForeground(Color.red);
                              
                  
                  TextField t1=new TextField();
+                 t1.setBackground(Color.CYAN);
                  TextField t2=new TextField();
+                 t2.setBackground(Color.CYAN);
                  TextField t3=new TextField();
+                 t3.setBackground(Color.CYAN);
                  TextField t4=new TextField();
+                 t4.setBackground(Color.CYAN);
                  
                  
                  Button b=new Button("Submit");
                  Button b1=new Button("Reset Data");
                  Button b2=new Button("Back");
+                 
+                 b.setBackground(Color.CYAN);
+                 b.setForeground(Color.black);
+                 
+                 b1.setBackground(Color.CYAN);
+                 b1.setForeground(Color.black);
+                 b2.setBackground(Color.CYAN);
+                 b2.setForeground(Color.black);
+                 
                  
                    l.setBounds(140,30,150,40);
                    l1.setBounds(30,100,120,40);
@@ -69,13 +101,28 @@ public class Attendance
                        public void actionPerformed(ActionEvent e)
                        {
                           Frame f=new Frame();
+                          f.setBackground(Color.lightGray);
+                          
+                          
                           Label l=new Label("DATA ENTERED SUCCESSFULLY");
+                          l.setForeground(Color.red);
                           Label l1=new Label();
+                          l.setForeground(Color.red);
                           Label l2=new Label();
+                          l.setForeground(Color.red);
                           Label l3=new Label();
+                          l.setForeground(Color.red);
                           Label l4=new Label();
+                          l.setForeground(Color.red);
+                          
+                          
                           Button b=new Button("Done");
                           Button b1=new Button("Back");
+                          
+                          b.setBackground(Color.CYAN);
+                          b.setForeground(Color.black);
+                          b1.setBackground(Color.CYAN);
+                          b1.setForeground(Color.black);
                           
                           l.setBounds(140,40,250,40);
                           l1.setBounds(30,100,180,40);
@@ -91,12 +138,16 @@ public class Attendance
                           
                           String s1="Name"+"  :    "+t1.getText();
                           l1.setText(s1);
+                          l1.setForeground(Color.red);
                           String s2="Id"+"    :    "+t2.getText();
                           l2.setText(s2);
+                          l2.setForeground(Color.red);
                           String s3="Time"+"  :    "+t3.getText();
                           l3.setText(s3);
+                          l3.setForeground(Color.red);
                           String s4="Date"+"  :    "+t4.getText();
                           l4.setText(s4);
+                          l4.setForeground(Color.red);
                           
                           b.addActionListener(new ActionListener()
                           {
@@ -178,21 +229,43 @@ public class Attendance
              public void actionPerformed(ActionEvent e)
              {
                  Frame f=new Frame("Attendance");
+                 f.setBackground(Color.lightGray);
+                 
                  Label l=new Label("Attendance Form");
+                 l.setForeground(Color.red);
                  Label l1=new Label("NAME : ");
+                 l1.setForeground(Color.red);
                  Label l2=new Label("ID   : ");
+                 l2.setForeground(Color.red);
                  Label l3=new Label("TIME : ");
+                 l3.setForeground(Color.red);
                  Label l4=new Label("DATE : ");
+                 l4.setForeground(Color.red);
                  
                  TextField t1=new TextField();
+                 t1.setBackground(Color.CYAN);
                  TextField t2=new TextField();
+                 t2.setBackground(Color.CYAN);
                  TextField t3=new TextField();
+                 t3.setBackground(Color.CYAN);
                  TextField t4=new TextField();
+                 t4.setBackground(Color.CYAN);
                  
+                 
+                
                  Button b=new Button("Submit");
                  Button b1=new Button("Reset Data");
                  Button b2=new Button("Back");
                  
+                  b.setBackground(Color.CYAN);
+                  b.setForeground(Color.black);
+                 
+                  b1.setBackground(Color.CYAN);
+                  b1.setForeground(Color.black);
+                  b2.setBackground(Color.CYAN);
+                  b2.setForeground(Color.black);
+                 
+                  
                  l.setBounds(140,30,150,40);
                  l1.setBounds(30,100,120,40);
                  l2.setBounds(30,150,120,40);
@@ -214,11 +287,19 @@ public class Attendance
                        public void actionPerformed(ActionEvent e)
                        {
                           Frame f=new Frame();
+                          f.setBackground(Color.lightGray);
+                          
                           Label l=new Label("DATA ENTERED SUCCESSFULLY");
+                          
                           Label l1=new Label();
+                           l1.setForeground(Color.red);
                           Label l2=new Label();
+                           l2.setForeground(Color.red);
                           Label l3=new Label();
+                           l3.setForeground(Color.red);
                           Label l4=new Label();
+                           l4.setForeground(Color.red);
+                           
                           Button b=new Button("Done");
                           Button b1=new Button("Back");
                           
@@ -230,18 +311,30 @@ public class Attendance
                   
                            b.setBounds(100,400,120,40);
                            b1.setBounds(250,400,120,40);
+                           
+                           b.setBackground(Color.CYAN);
+                           b.setForeground(Color.black);
+                           b1.setBackground(Color.CYAN);
+                           b1.setForeground(Color.black);
+                           
+                           
+                           
                           
                           f.add(l1);f.add(l2);f.add(l3);f.add(l4);f.add(l);f.add(b);f.add(b1);
                    
                           
                           String s1="Name"+"  :    "+t1.getText();
                           l1.setText(s1);
+                          l1.setForeground(Color.red);
                           String s2="Id"+"    :    "+t2.getText();
                           l2.setText(s2);
+                          l2.setForeground(Color.red);
                           String s3="Time"+"  :    "+t3.getText();
                           l3.setText(s3);
+                          l3.setForeground(Color.red);
                           String s4="Date"+"  :    "+t4.getText();
                           l4.setText(s4);
+                          l4.setForeground(Color.red);
                   
                             b.addActionListener(new ActionListener()
                           {
